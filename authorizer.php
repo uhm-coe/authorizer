4dfb291671a15c6599c2f917953d4616c3955b9c
Paul Ryan <paul.rar@gmail.com>
2014-06-13 10:15:30 -1000

    move uninstall routine to seperate file, per: http://codex.wordpress.org/Function_Reference/register_uninstall_hook

--- authorizer.php
+++ authorizer.php
@@ -64,7 +64,6 @@ if ( ! class_exists( 'WP_Plugin_Authorizer' ) ) {
 			// Installation and uninstallation hooks.
 			register_activation_hook( __FILE__, array( $this, 'activate' ) );
 			register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
-			register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
 
 			// Register filters.
 
@@ -195,52 +194,6 @@ if ( ! class_exists( 'WP_Plugin_Authorizer' ) ) {
 		public function deactivate() {
 			// Do nothing.
 		} // END deactivate()
-
-
-		/**
-		 * Plugin uninstallation. Runs when plugin is deleted (not disabled).
-		 *
-		 * @return void
-		 */
-		public function uninstall() {
-			// Delete options in database.
-			delete_option( 'auth_settings' );
-			delete_option( 'auth_settings_advanced_admin_notice' );
-			delete_option( 'auth_settings_advanced_login_error' );
-			delete_option( 'auth_settings_advanced_lockouts_time_last_failed' );
-			delete_option( 'auth_settings_advanced_lockouts_failed_attempts' );
-
-			// Delete multisite options
-			if ( is_multisite() ) {
-				delete_blog_option( BLOG_ID_CURRENT_SITE, 'auth_multisite_settings' );
-			}
-
-			// For security, delete inactive and blocked users (since we can't
-			// enforce their inactivity without this plugin enabled, which means
-			// they would be able to reset their passwords and log in). If they
-			// have any content, reassign it to the current user (the user
-			// uninstalling the plugin).
-			if ( ! is_multisite() ) {
-				$inactive_users = get_users( array(
-					'meta_key' => 'auth_inactive',
-					'meta_value' => 'yes',
-				));
-				$current_user = wp_get_current_user();
-				foreach ( $inactive_users as $inactive_user ) {
-					wp_delete_user( $inactive_user->ID, $current_user->ID );
-				}
-
-				$blocked_users = get_users( array(
-					'meta_key' => 'auth_blocked',
-					'meta_value' => 'yes',
-				));
-				$current_user = wp_get_current_user();
-				foreach ( $blocked_users as $blocked_user ) {
-					wp_delete_user( $blocked_user->ID, $current_user->ID );
-				}
-			}
-
-		} // END uninstall()
 
 
 
---
+++ uninstall.php
@@ -0,0 +1,43 @@
+<?php
+
+// Exit if uninstall not called from WordPress.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )  {
+	exit();
+}
+
+// Delete options in database.
+delete_option( 'auth_settings' );
+delete_option( 'auth_settings_advanced_admin_notice' );
+delete_option( 'auth_settings_advanced_login_error' );
+delete_option( 'auth_settings_advanced_lockouts_time_last_failed' );
+delete_option( 'auth_settings_advanced_lockouts_failed_attempts' );
+
+// Delete multisite options
+if ( is_multisite() ) {
+	delete_blog_option( BLOG_ID_CURRENT_SITE, 'auth_multisite_settings' );
+}
+
+// For security, delete inactive and blocked users (since we can't
+// enforce their inactivity without this plugin enabled, which means
+// they would be able to reset their passwords and log in). If they
+// have any content, reassign it to the current user (the user
+// uninstalling the plugin).
+if ( ! is_multisite() ) {
+	$inactive_users = get_users( array(
+		'meta_key' => 'auth_inactive',
+		'meta_value' => 'yes',
+	));
+	$current_user = wp_get_current_user();
+	foreach ( $inactive_users as $inactive_user ) {
+		wp_delete_user( $inactive_user->ID, $current_user->ID );
+	}
+
+	$blocked_users = get_users( array(
+		'meta_key' => 'auth_blocked',
+		'meta_value' => 'yes',
+	));
+	$current_user = wp_get_current_user();
+	foreach ( $blocked_users as $blocked_user ) {
+		wp_delete_user( $blocked_user->ID, $current_user->ID );
+	}
+}
