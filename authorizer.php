ac8949068fe7b452973e37496dc7100dd9acb0df
Paul Ryan <paul.rar@gmail.com>
2016-11-15 11:08:51 -1000

    Explicitly declare jquery as a dependency for authorizer-public.js

--- authorizer.php
+++ authorizer.php
@@ -1613,7 +1613,7 @@ if ( ! class_exists( 'WP_Plugin_Authorizer' ) ) {
 		function auth_public_scripts() {
 			// Load (and localize) public scripts
 			$current_path = empty( $_SERVER['REQUEST_URI'] ) ? home_url() : $_SERVER['REQUEST_URI'];
-			wp_enqueue_script( 'auth_public_scripts', plugins_url( '/js/authorizer-public.js', __FILE__ ), array(), '2.3.2' );
+			wp_enqueue_script( 'auth_public_scripts', plugins_url( '/js/authorizer-public.js', __FILE__ ), array( 'jquery' ), '2.3.2' );
 			$auth_localized = array(
 				'wp_login_url' => wp_login_url( $current_path ),
 				'public_warning' => get_option( 'auth_settings_advanced_public_notice' )
