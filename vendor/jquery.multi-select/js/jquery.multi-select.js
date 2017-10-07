27f8913902d3672d0a35f78283d6c09ad2505c53
Paul Ryan <paul.rar@gmail.com>
2017-10-06 16:08:26 -1000

    Update jquery.multi-select from 0.9.8 to 0.9.12

--- vendor/jquery.multi-select/js/jquery.multi-select.js
+++ vendor/jquery.multi-select/js/jquery.multi-select.js
@@ -1,5 +1,5 @@
 /*
-* MultiSelect v0.9.8
+* MultiSelect v0.9.12
 * Copyright (c) 2012 Louis Cuny
 *
 * This program is free software. It comes without any warranty, to
@@ -20,14 +20,12 @@
   var MultiSelect = function (element, options) {
     this.options = options;
     this.$element = $(element);
-
     this.$container = $('<div/>', { 'class': "ms-container" });
     this.$selectableContainer = $('<div/>', { 'class': 'ms-selectable' });
     this.$selectionContainer = $('<div/>', { 'class': 'ms-selection' });
     this.$selectableUl = $('<ul/>', { 'class': "ms-list", 'tabindex' : '-1' });
     this.$selectionUl = $('<ul/>', { 'class': "ms-list", 'tabindex' : '-1' });
     this.scrollTo = 0;
-    this.sanitizeRegexp = new RegExp("\\W+", 'gi');
     this.elemsSelector = 'li:visible:not(.ms-optgroup-label,.ms-optgroup-container,.'+options.disabledClass+')';
   };
 
@@ -42,7 +40,7 @@
         ms.css({ position: 'absolute', left: '-9999px' });
         ms.attr('id', ms.attr('id') ? ms.attr('id') : Math.ceil(Math.random()*1000)+'multiselect');
         this.$container.attr('id', 'ms-'+ms.attr('id'));
-
+        this.$container.addClass(that.options.cssClass);
         ms.find('option').each(function(){
           that.generateLisFromOption(this);
         });
@@ -86,7 +84,7 @@
 
         ms.on('focus', function(){
           that.$selectableUl.focus();
-        })
+        });
       }
 
       var selectedValues = ms.find('option:selected').map(function(){ return $(this).val(); }).get();
@@ -97,7 +95,7 @@
       }
     },
 
-    'generateLisFromOption' : function(option){
+    'generateLisFromOption' : function(option, index, $container){
       var that = this,
           ms = that.$element,
           attributes = "",
@@ -106,14 +104,14 @@
       for (var cpt = 0; cpt < option.attributes.length; cpt++){
         var attr = option.attributes[cpt];
 
-        if(attr.name !== 'value'){
+        if(attr.name !== 'value' && attr.name !== 'disabled'){
           attributes += attr.name+'="'+attr.value+'" ';
         }
       }
-      var selectableLi = $('<li '+attributes+'><span>'+$option.text()+'</span></li>'),
+      var selectableLi = $('<li '+attributes+'><span>'+that.escapeHTML($option.text())+'</span></li>'),
           selectedLi = selectableLi.clone(),
           value = $option.val(),
-          elementId = that.sanitize(value, that.sanitizeRegexp);
+          elementId = that.sanitize(value);
 
       selectableLi
         .data('ms-value', value)
@@ -135,14 +133,14 @@
 
       if ($optgroup.length > 0){
         var optgroupLabel = $optgroup.attr('label'),
-            optgroupId = that.sanitize(optgroupLabel, that.sanitizeRegexp),
+            optgroupId = that.sanitize(optgroupLabel),
             $selectableOptgroup = that.$selectableUl.find('#optgroup-selectable-'+optgroupId),
             $selectionOptgroup = that.$selectionUl.find('#optgroup-selection-'+optgroupId);
-        
+
         if ($selectableOptgroup.length === 0){
           var optgroupContainerTpl = '<li class="ms-optgroup-container"></li>',
               optgroupTpl = '<ul class="ms-optgroup"><li class="ms-optgroup-label"><span>'+optgroupLabel+'</span></li></ul>';
-          
+
           $selectableOptgroup = $(optgroupContainerTpl);
           $selectionOptgroup = $(optgroupContainerTpl);
           $selectableOptgroup.attr('id', 'optgroup-selectable-'+optgroupId);
@@ -151,23 +149,57 @@
           $selectionOptgroup.append($(optgroupTpl));
           if (that.options.selectableOptgroup){
             $selectableOptgroup.find('.ms-optgroup-label').on('click', function(){
-              var values = $optgroup.children(':not(:selected)').map(function(){ return $(this).val() }).get();
+              var values = $optgroup.children(':not(:selected, :disabled)').map(function(){ return $(this).val();}).get();
               that.select(values);
             });
             $selectionOptgroup.find('.ms-optgroup-label').on('click', function(){
-              var values = $optgroup.children(':selected').map(function(){ return $(this).val() }).get();
+              var values = $optgroup.children(':selected:not(:disabled)').map(function(){ return $(this).val();}).get();
               that.deselect(values);
             });
           }
           that.$selectableUl.append($selectableOptgroup);
           that.$selectionUl.append($selectionOptgroup);
         }
-        $selectableOptgroup.children().append(selectableLi);
-        $selectionOptgroup.children().append(selectedLi);
+        index = index === undefined ? $selectableOptgroup.find('ul').children().length : index + 1;
+        selectableLi.insertAt(index, $selectableOptgroup.children());
+        selectedLi.insertAt(index, $selectionOptgroup.children());
       } else {
-        that.$selectableUl.append(selectableLi);
-        that.$selectionUl.append(selectedLi);
+        index = index === undefined ? that.$selectableUl.children().length : index;
+
+        selectableLi.insertAt(index, that.$selectableUl);
+        selectedLi.insertAt(index, that.$selectionUl);
       }
+    },
+
+    'addOption' : function(options){
+      var that = this;
+
+      if (options.value !== undefined && options.value !== null){
+        options = [options];
+      } 
+      $.each(options, function(index, option){
+        if (option.value !== undefined && option.value !== null &&
+            that.$element.find("option[value='"+option.value+"']").length === 0){
+          var $option = $('<option value="'+option.value+'">'+option.text+'</option>'),
+              $container = option.nested === undefined ? that.$element : $("optgroup[label='"+option.nested+"']"),
+              index = parseInt((typeof option.index === 'undefined' ? $container.children().length : option.index));
+
+          if (option.optionClass) {
+            $option.addClass(option.optionClass);
+          }
+
+          if (option.disabled) {
+            $option.prop('disabled', true);
+          }
+
+          $option.insertAt(index, $container);
+          that.generateLisFromOption($option.get(0), index, option.nested);
+        }
+      });
+    },
+
+    'escapeHTML' : function(text){
+      return $("<div>").text(text).html();
     },
 
     'activeKeyboard' : function($list){
@@ -187,17 +219,30 @@
             e.stopPropagation();
             that.moveHighlight($(this), (e.which === 38) ? -1 : 1);
             return;
-          case 32:
-            e.preventDefault();
-            e.stopPropagation();
-            that.selectHighlighted($list);
-            return;
           case 37:
           case 39:
             e.preventDefault();
             e.stopPropagation();
             that.switchList($list);
             return;
+          case 9:
+            if(that.$element.is('[tabindex]')){
+              e.preventDefault();
+              var tabindex = parseInt(that.$element.attr('tabindex'), 10);
+              tabindex = (e.shiftKey) ? tabindex-1 : tabindex+1;
+              $('[tabindex="'+(tabindex)+'"]').focus();
+              return;
+            }else{
+              if(e.shiftKey){
+                that.$element.trigger('focus');
+              }
+            }
+        }
+        if($.inArray(e.which, that.options.keySelect) > -1){
+          e.preventDefault();
+          e.stopPropagation();
+          that.selectHighlighted($list);
+          return;
         }
       });
     },
@@ -209,10 +254,6 @@
           elemHeight = $elems.first().outerHeight(),
           containerHeight = $list.height(),
           containerSelector = '#'+this.$container.prop('id');
-
-      // Deactive mouseenter event when move is active
-      // It fixes a bug when mouse is over the list
-      $elems.off('mouseenter');
 
       $elems.removeClass('ms-hover');
       if (direction === 1){ // DOWN
@@ -264,21 +305,22 @@
     },
 
     'selectHighlighted' : function($list){
-        var $elems = $list.find(this.elemsSelector),
-            $highlightedElem = $elems.filter('.ms-hover').first();
+      var $elems = $list.find(this.elemsSelector),
+          $highlightedElem = $elems.filter('.ms-hover').first();
 
-        if ($highlightedElem.length > 0){
-          if ($list.parent().hasClass('ms-selectable')){
-            this.select($highlightedElem.data('ms-value'));
-          } else {
-            this.deselect($highlightedElem.data('ms-value'));
-          }
-          $elems.removeClass('ms-hover');
+      if ($highlightedElem.length > 0){
+        if ($list.parent().hasClass('ms-selectable')){
+          this.select($highlightedElem.data('ms-value'));
+        } else {
+          this.deselect($highlightedElem.data('ms-value'));
         }
+        $elems.removeClass('ms-hover');
+      }
     },
 
     'switchList' : function($list){
       $list.blur();
+      this.$container.find(this.elemsSelector).removeClass('ms-hover');
       if ($list.parent().hasClass('ms-selectable')){
         this.$selectionUl.focus();
       } else {
@@ -289,13 +331,13 @@
     'activeMouse' : function($list){
       var that = this;
 
-      $list.on('mousemove', function(){
-        var elems = $list.find(that.elemsSelector);
+      this.$container.on('mouseenter', that.elemsSelector, function(){
+        $(this).parents('.ms-container').find(that.elemsSelector).removeClass('ms-hover');
+        $(this).addClass('ms-hover');
+      });
 
-        elems.on('mouseenter', function(){
-          elems.removeClass('ms-hover');
-          $(this).addClass('ms-hover');
-        });
+      this.$container.on('mouseleave', that.elemsSelector, function () {
+        $(this).parents('.ms-container').find(that.elemsSelector).removeClass('ms-hover');
       });
     },
 
@@ -306,6 +348,8 @@
 
     'destroy' : function(){
       $("#ms-"+this.$element.attr("id")).remove();
+      this.$element.off('focus');
+      this.$element.css('position', '').css('left', '');
       this.$element.removeData('multiselect');
     },
 
@@ -314,15 +358,23 @@
 
       var that = this,
           ms = this.$element,
-          msIds = $.map(value, function(val){ return(that.sanitize(val, that.sanitizeRegexp)); }),
+          msIds = $.map(value, function(val){ return(that.sanitize(val)); }),
           selectables = this.$selectableUl.find('#' + msIds.join('-selectable, #')+'-selectable').filter(':not(.'+that.options.disabledClass+')'),
           selections = this.$selectionUl.find('#' + msIds.join('-selection, #') + '-selection').filter(':not(.'+that.options.disabledClass+')'),
           options = ms.find('option:not(:disabled)').filter(function(){ return($.inArray(this.value, value) > -1); });
 
+      if (method === 'init'){
+        selectables = this.$selectableUl.find('#' + msIds.join('-selectable, #')+'-selectable'),
+        selections = this.$selectionUl.find('#' + msIds.join('-selection, #') + '-selection');
+      }
+
       if (selectables.length > 0){
         selectables.addClass('ms-selected').hide();
         selections.addClass('ms-selected').show();
+
         options.prop('selected', true);
+
+        that.$container.find(that.elemsSelector).removeClass('ms-hover');
 
         var selectableOptgroups = that.$selectableUl.children('.ms-optgroup-container');
         if (selectableOptgroups.length > 0){
@@ -341,8 +393,11 @@
             }
           });
         } else {
-          if (that.options.keepOrder){
-            selections.insertAfter(that.$selectionUl.find('.ms-selected').last());  
+          if (that.options.keepOrder && method !== 'init'){
+            var selectionLiLast = that.$selectionUl.find('.ms-selected');
+            if((selectionLiLast.length > 1) && (selectionLiLast.last().get(0) != selections.get(0))) {
+              selections.insertAfter(selectionLiLast.last());
+            }
           }
         }
         if (method !== 'init'){
@@ -359,15 +414,17 @@
 
       var that = this,
           ms = this.$element,
-          msIds = $.map(value, function(val){ return(that.sanitize(val, that.sanitizeRegexp)); }),
+          msIds = $.map(value, function(val){ return(that.sanitize(val)); }),
           selectables = this.$selectableUl.find('#' + msIds.join('-selectable, #')+'-selectable'),
-          selections = this.$selectionUl.find('#' + msIds.join('-selection, #')+'-selection').filter('.ms-selected'),
+          selections = this.$selectionUl.find('#' + msIds.join('-selection, #')+'-selection').filter('.ms-selected').filter(':not(.'+that.options.disabledClass+')'),
           options = ms.find('option').filter(function(){ return($.inArray(this.value, value) > -1); });
 
       if (selections.length > 0){
         selectables.removeClass('ms-selected').show();
         selections.removeClass('ms-selected').hide();
         options.prop('selected', false);
+
+        that.$container.find(that.elemsSelector).removeClass('ms-hover');
 
         var selectableOptgroups = that.$selectableUl.children('.ms-optgroup-container');
         if (selectableOptgroups.length > 0){
@@ -428,8 +485,16 @@
       }
     },
 
-    sanitize: function(value, reg){
-      return(value.replace(reg, '_'));
+    sanitize: function(value){
+      var hash = 0, i, character;
+      if (value.length == 0) return hash;
+      var ls = 0;
+      for (i = 0, ls = value.length; i < ls; i++) {
+        character  = value.charCodeAt(i);
+        hash  = ((hash<<5)-hash)+character;
+        hash |= 0; // Convert to 32bit integer
+      }
+      return hash;
     }
   };
 
@@ -456,12 +521,24 @@
   };
 
   $.fn.multiSelect.defaults = {
+    keySelect: [32],
     selectableOptgroup: false,
     disabledClass : 'disabled',
     dblClick : false,
-    keepOrder: false
+    keepOrder: false,
+    cssClass: ''
   };
 
   $.fn.multiSelect.Constructor = MultiSelect;
+
+  $.fn.insertAt = function(index, $parent) {
+    return this.each(function() {
+      if (index === 0) {
+        $parent.prepend(this);
+      } else {
+        $parent.children().eq(index - 1).after(this);
+      }
+    });
+};
 
 }(window.jQuery);
--- vendor/jquery.multi-select/test/spec/multiSelectSpec.js
+++ vendor/jquery.multi-select/test/spec/multiSelectSpec.js
@@ -60,6 +60,66 @@ describe("multiSelect", function() {
         expect($('.ms-selectable ul.ms-list li.ms-selected').length).toEqual(2);
       });
     });
+
+    describe("with disabled pre-selected options", function(){
+      var selectedValues = ['value1', 'value2', 'value3'];
+
+      beforeEach(function() {
+        $('#multi-select').find('option')
+          .first().prop('selected', true).prop('disabled', true)
+          .next().prop('selected', true)
+          .next().prop('selected', true).prop('disabled', true)
+        ;
+        $('#multi-select').multiSelect();
+      })
+
+      it ('should select the disabled pre-selected options', function(){
+        $.each(selectedValues, function(index, value){
+          expect($('.ms-selectable ul.ms-list li#'+sanitize(value)+'-selectable')).toBe('.ms-selected');
+        });
+        expect($('.ms-selectable ul.ms-list li.ms-selected').length).toEqual(3);
+      });
+    });
+
+    describe("with disabled non-selected options", function(){
+      var selectedValues = ['value1', 'value3'];
+
+      beforeEach(function() {
+        $('#multi-select').find('option')
+          .first().prop('selected', true)
+          .next().prop('disabled', true)
+          .next().prop('selected', true)
+        ;
+        $('#multi-select').multiSelect();
+      })
+
+      it ('should not select the disabled non-selected options', function(){
+        $.each(selectedValues, function(index, value){
+          expect($('.ms-selectable ul.ms-list li#'+sanitize(value)+'-selectable')).toBe('.ms-selected');
+        });
+        expect($('.ms-selectable ul.ms-list li.ms-selected').length).toEqual(2);
+      });
+    });
+  });
+
+  describe('destroy', function(){
+
+    describe('destroy multi select', function(){
+      beforeEach(function(){
+        select.multiSelect();
+        msContainer = select.next();
+        select.multiSelect('destroy');
+      });
+
+      it('should show the original select', function(){
+        expect(select.css('position')).not.toBe('absolute');
+        expect(select.css('left')).not.toBe('-9999px');
+      });
+
+      it('should destroy the multiSelect container', function(){
+        expect(select.next().size()).toEqual(0);
+      });
+    });
   });
 
   describe('optgroup', function(){
@@ -177,6 +237,44 @@ describe("multiSelect", function() {
         select.multiSelect('deselect_all');
       });
     });
+
+    describe("on click on disabled non-selected option", function(){
+      var clickedItem, value;
+
+      beforeEach(function() {
+        $('#multi-select').find('option').first().prop('disabled', true);
+        $('#multi-select').multiSelect();
+        clickedItem = $('.ms-selectable ul.ms-list li').first();
+        value = clickedItem.data('ms-value');
+        spyOnEvent(select, 'change');
+        spyOnEvent(select, 'focus');
+        clickedItem.trigger('click');
+      });
+
+      it('should not hide selected item', function(){
+        expect(clickedItem).not.toBeHidden();
+      });
+
+      it('should not add the .ms-selected class to the selected item', function(){
+        expect(clickedItem.hasClass('ms-selected')).not.toBeTruthy();
+      });
+
+      it('should not select corresponding option', function(){
+        expect(select.find('option[value="'+value+'"]')).not.toBeSelected();
+      });
+
+      it('should not show the associated selected item', function(){
+        expect($('#'+sanitize(value)+'-selection')).not.toBe(':visible');
+      });
+
+      it('should not trigger the original select change event', function(){
+        expect('change').not.toHaveBeenTriggeredOn("#multi-select");
+      });
+
+      afterEach(function(){
+        select.multiSelect('deselect_all');
+      });
+    });
   });
 
   describe('deselect', function(){
@@ -218,7 +316,7 @@ describe("multiSelect", function() {
         $('#multi-select').multiSelect();
 
         clickedItem = $('.ms-selection ul.ms-list li').first();
-        value = clickedItem.attr('id').replace('-selection', '');
+        value = clickedItem.data('ms-value');
         correspondingSelectableItem = $('.ms-selection ul.ms-list li').first();
         spyOnEvent(select, 'change');
         spyOnEvent(select, 'focus');
@@ -230,7 +328,7 @@ describe("multiSelect", function() {
       });
 
       it('should show associated selectable item', function(){
-        expect($('#'+value+'-selectable')).toBe(':visible');
+        expect($('#'+sanitize(value)+'-selectable')).toBe(':visible');
       });
 
       it('should remove the .ms-selected class to the corresponding selectable item', function(){
@@ -243,6 +341,47 @@ describe("multiSelect", function() {
 
       it('should trigger the original select change event', function(){
         expect('change').toHaveBeenTriggeredOn("#multi-select");
+      });
+
+      afterEach(function(){
+        select.multiSelect('deselect_all');
+      });
+    });
+
+    describe("on click on disabled selected option", function(){
+      var clickedItem, value;
+      var correspondingSelectableItem;
+
+      beforeEach(function() {
+        $('#multi-select').find('option').first().prop('selected', true).prop('disabled', true);
+        $('#multi-select').multiSelect();
+
+        clickedItem = $('.ms-selection ul.ms-list li').first();
+        value = clickedItem.data('ms-value');
+        correspondingSelectableItem = $('.ms-selection ul.ms-list li').first();
+        spyOnEvent(select, 'change');
+        spyOnEvent(select, 'focus');
+        clickedItem.trigger('click');
+      });
+
+      it ('should not hide clicked item', function(){
+        expect(clickedItem).not.toBe(':hidden');
+      });
+
+      it('should not show associated selectable item', function(){
+        expect($('#'+value+'-selectable')).not.toBe(':visible');
+      });
+
+      it('should not remove the .ms-selected class to the corresponding selectable item', function(){
+        expect(correspondingSelectableItem.hasClass('ms-selected')).not.toBeFalsy();
+      });
+
+      it('should not deselect corresponding option', function(){
+        expect(select.find('option[value="'+value+'"]')).toBeSelected();
+      });
+
+      it('should not trigger the original select change event', function(){
+        expect('change').not.toHaveBeenTriggeredOn("#multi-select");
       });
 
       afterEach(function(){
