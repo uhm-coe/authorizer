b64d866e6ccb85f8ecc02d6b78e53d5e10735556
Paul Ryan <paul.rar@gmail.com>
2018-06-15 15:50:50 -1000

    Coding standards (authorizer-login.js)

--- js/authorizer-login.js
+++ js/authorizer-login.js
@@ -1,55 +1,68 @@
-jQuery( document ).ready( function( $ ) {
-	// Move any external service buttons into top of login form.
-	var loginform = document.getElementById( 'loginform' );
-	var externals = document.getElementById( 'auth-external-service-login' );
-	if ( loginform !== null && externals !== null ) {
-		loginform.insertBefore( externals, loginform.firstChild );
-	}
+/**
+ * Small style tweaks for login form, and show a counter if user triggered login
+ * protections (too many failed attempts).
+ */
 
-	// Decrement seconds counter if it exists
-	var seconds_element = document.getElementById( 'seconds_remaining' );
-	if ( seconds_element !== null ) {
-		var seconds_interval = setInterval( function() {
-			var seconds = seconds_element.getAttribute( 'data-seconds' );
-			if ( seconds < 1 ) {
-				clearInterval( 'seconds_interval' );
-				return;
-			}
-			seconds = parseInt( seconds ) - 1;
-			seconds_element.innerHTML = seconds_as_sentence( seconds );
-			seconds_element.setAttribute( 'data-seconds', seconds );
-		}, 1000 );
-	}
-});
+/* global document */
+/* eslint-env browser */
+( function( $ ) {
 
-function seconds_as_sentence( seconds ) {
-	var units = {
-		week   : 7 * 24 * 3600,
-		day    :     24 * 3600,
-		hour   :          3600,
-		minute :            60,
-		second :             1
-	};
-
-	// specifically handle zero
-	if ( seconds === 0 ) {
-		return '0 seconds';
-	}
-
-	// Construct sentence, e.g., '1 week, 2 hours, 5 minutes, 10 seconds, '
-	var s = '';
-	for ( var name in units ) {
-		var divisor = units[name];
-		var quot = Math.floor( seconds / divisor );
-		if ( quot ) {
-			s += quot + ' ' + name;
-			if ( Math.abs( quot ) > 1 ) {
-				s += 's';
-			}
-			s += ', ';
-			seconds -= quot * divisor;
+	$( document ).ready( function() {
+		// Move any external service buttons into top of login form.
+		var loginform = document.getElementById( 'loginform' );
+		var externals = document.getElementById( 'auth-external-service-login' );
+		if ( null !== loginform && null !== externals ) {
+			loginform.insertBefore( externals, loginform.firstChild );
 		}
+
+		// Decrement seconds counter if it exists
+		var secondsElement = document.getElementById( 'seconds_remaining' );
+		if ( null !== secondsElement ) {
+			var secondsInterval = setInterval( function() {
+				var seconds = secondsElement.getAttribute( 'data-seconds' );
+				if ( 1 > seconds ) {
+					clearInterval( secondsInterval );
+					return;
+				}
+				seconds = parseInt( seconds, 10 ) - 1;
+				secondsElement.innerHTML = secondsAsSentence( seconds );
+				secondsElement.setAttribute( 'data-seconds', seconds );
+			}, 1000 );
+		}
+	});
+
+	function secondsAsSentence( seconds ) {
+		var units = {
+			week: 3600 * 24 * 7,
+			day: 3600 * 24,
+			hour: 3600,
+			minute: 60,
+			second: 1,
+		};
+
+		// specifically handle zero
+		if ( 0 === seconds ) {
+			return '0 seconds';
+		}
+
+		// Construct sentence, e.g., '1 week, 2 hours, 5 minutes, 10 seconds, '
+		var phrase = '';
+		for ( var name in units ) {
+			if ( units.hasOwnProperty( name ) ) {
+				var divisor = units[name];
+				var quot = Math.floor( seconds / divisor );
+				if ( quot ) {
+					phrase += quot + ' ' + name;
+					if ( 1 < Math.abs( quot ) ) {
+						phrase += 's';
+					}
+					phrase += ', ';
+					seconds -= quot * divisor;
+				}
+			}
+		}
+
+		return phrase.substring( 0, phrase.length - 2 ); // trim off last ', '
 	}
 
-	return s.substring( 0, s.length - 2 ); // trim off last ', '
-}
+} )( jQuery );
