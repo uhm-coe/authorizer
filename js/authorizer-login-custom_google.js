394f306cdaeb8d480653ee098f3b64a3691b86a5
Paul Ryan <paul.rar@gmail.com>
2018-06-20 14:00:19 -1000

    Coding standards (authorizer-login-custom_google.js)

--- js/authorizer-login-custom_google.js
+++ js/authorizer-login-custom_google.js
@@ -1,21 +1,25 @@
-(function () {
+/**
+ * Inserts the "Sign in with Google" button.
+ */
+
+/* global document, signInCallback, gapi */
+( function() {
 	var po = document.createElement( 'script' );
 	po.type = 'text/javascript';
 	po.async = true;
 	po.src = 'https://plus.google.com/js/client:plusone.js?onload=render';
-	var s = document.getElementsByTagName( 'script' )[0];
-	s.parentNode.insertBefore( po, s );
+	var scr = document.getElementsByTagName( 'script' )[0];
+	scr.parentNode.insertBefore( po, scr );
 })();
 
 /* Executed when the APIs finish loading */
-function render() {
-
+// eslint-disable-next-line
+function render() { // jshint ignore:line
 	// Additional params including the callback, the rest of the params will
 	// come from the page-level configuration.
 	var additionalParams = {
-		'callback': signInCallback
+		callback: signInCallback,
 	};
-
 	// Attach a click listener to a button to trigger the flow.
 	var signinButton = document.getElementById( 'googleplus_button' );
 	signinButton.addEventListener( 'click', function() {
