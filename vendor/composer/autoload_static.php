a71d78254961afb3e39b87775cb55be8d664b668
Paul Ryan <paul.rar@gmail.com>
2023-11-01 11:32:56 -1000

    Update composer dependencies
    google/apiclient (v2.13.2 => v2.14.0)
    thenetworg/oauth2-azure (v2.1.1 => v2.2.1)
    components/jquery (3.6.0 => v3.7.1)
    google/apiclient-services (v0.297.0 => v0.302.0)
    guzzlehttp/guzzle (7.5.1 => 7.8.0)
    guzzlehttp/promises (1.5.2 => 2.0.1)
    guzzlehttp/psr7 (2.5.0 => 2.6.1)
    monolog/monolog (2.9.1 => 2.9.2)
    phpseclib/phpseclib (3.0.19 => 3.0.33)
    psr/http-client (1.0.2 => 1.0.3)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,7 +8,6 @@ class ComposerStaticInit3afe4a77f11b1a9be67c8c9e1abad910
 {
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         '1f87db08236948d07391152dccb70f04' => __DIR__ . '/..' . '/google/apiclient-services/autoload.php',
