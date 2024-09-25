9fe6e313fd1d58503b0a1a64d986c370c2d97b18
Paul Ryan <paul.rar@gmail.com>
2024-09-24 15:38:46 -1000

    Force Google logins to use FedCM
    for support of upcoming deprecation of 3rd party cookies in chromium browsers.
    fixes #157

--- src/authorizer/class-login-form.php
+++ src/authorizer/class-login-form.php
@@ -187,6 +187,7 @@ function signInCallback( credentialResponse ) { // jshint ignore:line
 			<?php if ( '1' === $auth_settings['google'] ) : ?>
 				<script src="https://accounts.google.com/gsi/client" async defer></script>
 				<div id="g_id_onload"
+					data-use_fedcm_for_prompt="true"
 					data-client_id="<?php echo esc_attr( trim( $auth_settings['google_clientid'] ) ); ?>"
 					data-context="signin"
 					data-ux_mode="popup"
